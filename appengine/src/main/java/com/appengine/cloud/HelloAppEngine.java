d906844c998c4b6773ea91eec532288990ae842c
rickypandi <pandi.naga@anywhere.co>
2019-09-14 10:25:32 +0530

    updated

--- appengine/src/main/java/com/appengine/cloud/HelloAppEngine.java
+++ appengine/src/main/java/com/appengine/cloud/HelloAppEngine.java
@@ -7,16 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
 @WebServlet(
     name = "Hello USer",
     urlPatterns = {"/hello"}
 )
-public class HelloAppEngine extends HttpServlet {
+public class HelloAppEngine extends HttpServlet{
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
-
+      throws IOException{
+	  DatastoreService ds;
+	  ds=DatastoreServiceFactory.getDatastoreService();
+	  //entity without identifier
+	  Entity e=new Entity("Employee");
+	  e.setProperty("Name","pandikumar");
+	  e.setProperty("emp Id","1004");
+	  ds.put(e);
+	  //entity with identifier
+	  Entity user=new Entity("emp","01");
+	  user.setProperty("name", "ricky");
+	  user.setProperty("emp id", "1005");
+	  ds.put(user);
+	  
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
 
